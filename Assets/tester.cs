ca3ffce68712e5e3c9270327d7d1470dac484378
hyperbting <yenkuoka@usc.edu>
2016-07-10 10:16:21 +0800

    unity project base

---
+++ Assets/tester.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class tester : MonoBehaviour 
+{
+
+	// Use this for initialization
+    void Start()
+    {
+        StartCoroutine("Tester");
+    }
+
+    public IEnumerator Tester()
+    {
+        Action a = null;
+        for (int i = 0; i < 3; i++)
+        {
+            int temp = i;
+            Action aa = () => { Debug.Log(Time.time + " temp=" + temp + " " + temp.GetHashCode() + "\n"); };
+
+            a += aa;
+            yield return new WaitForSeconds(1);
+            Debug.Log("for loop " + temp);
+            a();//Debug Purpose
+            Debug.Log("---end for loop " + temp);
+        }
+        Debug.Log("----------");
+        a();
+        yield break;
+    }
+
+}
---
+++ Assets/tester2.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class tester2 : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine("Tester2");
+    }
+
+    public IEnumerator Tester2()
+    {
+        AnotherFunction();
+        yield break;
+    }
+
+    public void AnotherFunction()
+    {
+        Action a = null;
+
+        for (int i = 0; i < 3; i++)
+        {
+            string temp = i.ToString();
+            a += () => { Debug.Log(Time.time + " temp=" + temp + " " + temp.GetHashCode()); };
+            Debug.Log("for loop " + temp);
+            a();//Debug Purpose
+            Debug.Log("---end for loop " + temp);
+        }
+        Debug.Log("----------");
+        a();
+    }
+}
